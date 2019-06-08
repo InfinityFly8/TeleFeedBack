3e00849680306e21c71da28349a6989889249700
InfintyFly8 <infintyfly8@tutanota.com>
2019-06-08 15:49:53 +0300

    Added exceptions and bot info logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import json
 from types import SimpleNamespace
 
@@ -8,31 +9,50 @@ with open('settings.json') as file:
     settings = SimpleNamespace(**json.load(file))
 admin_id = settings.ADMIN_ID
 bot = telebot.TeleBot(settings.API_TOKEN)
-print(bot.get_me())
+
+#logging init
+logging.basicConfig()
+logger = logging.getLogger('feedback bot')
+logger.setLevel(logging.INFO)
+
 # log about bot
+bot_info = '\n' + '\n'.join(f"{(k+':').ljust(14)}  {v}" for k, v in 
+                            bot.get_me().__dict__.items())
+logger.info(bot_info)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def hello(message):
-    if message.chat.id == admin_id:
-        bot.send_message(admin_id, settings.HELLO_ADMIN)
-    else:
-        bot.send_message(message.chat.id, settings.HELLO_CHAT)
+    try:
+        if message.chat.id == admin_id:
+            bot.send_message(admin_id, settings.HELLO_ADMIN)
+        else:
+            bot.send_message(message.chat.id, settings.HELLO_CHAT)
+    except:
+        logger.exception('Sending Error!')
 
 
 @bot.message_handler(func=lambda mess: mess.from_user.id == admin_id)
 def handle_admin_messages(message: types.Message):
     if message.reply_to_message is None:
-        bot.send_message(admin_id, settings.NO_REPLY_MESSAGE)
+        try:
+            bot.send_message(admin_id, settings.NO_REPLY_MESSAGE)
+        except:
+            logger.exception('Sending Error')
         return
-    chat_id = message.reply_to_message.forward_from.id
-    # TODO: add other types
-    bot.send_message(chat_id, message.text, parse_mode='Markdown')
-
+    try:
+        chat_id = message.reply_to_message.forward_from.id
+        # TODO: add other types
+        bot.send_message(chat_id, message.text, parse_mode='Markdown')
+    except:
+        logger.exception('Replying Error!')
 
 @bot.message_handler(func=lambda mess: mess.from_user.id != admin_id)
 def handle_other_messages(message: types.Message):
-    bot.forward_message(admin_id, message.chat.id, message.message_id)
+    try:
+        bot.forward_message(admin_id, message.chat.id, message.message_id)
+    except:
+        logger.exception('Forwarding Error!')
 
 
 if __name__ == '__main__':
