7147c776f43831e9596638353bdc9ea615abe9ee
InfintyFly8 <infintyfly8@tutanota.com>
2019-06-13 22:41:53 +0300

    added more message types

--- main.py
+++ main.py
@@ -21,6 +21,43 @@ bot_info = '\n' + '\n'.join(f"{(k+':').ljust(14)}  {v}" for k, v in
 logger.info(bot_info)
 
 
+class AllTypes:
+    def __contains__(*args):
+        return True
+
+
+def send_message_content(id, message):
+    # text, audio, document, photo, sticker, video, video_note, voice, invoice
+    if message.content_type == 'text':
+        bot.send_message(id, message.text)
+
+    elif message.content_type == 'audio':
+        bot.send_audio(id, message.audio.file_id, caption=message.caption)
+
+    elif message.content_type == 'document':
+        bot.send_document(id, message.document.file_id, caption=message.caption)
+
+    elif message.content_type == 'photo':
+        bot.send_photo(id, message.photo[-1].file_id, caption=message.caption)
+
+    elif message.content_type == 'sticker':
+        bot.send_sticker(id, message.sticker.file_id)
+
+    elif message.content_type == 'video':
+        bot.send_video(id, message.video.file_id, caption=message.caption)
+
+    elif message.content_type == 'video_note':
+        bot.send_video_note(id, message.video_note.file_id)
+
+    elif message.content_type == 'voice':
+        bot.send_voice(id, message.voice.file_id, caption=message.caption)
+
+    elif message.content_type == 'invoice':
+        bot.send_invoice(id, message.invoice.file_id)
+    else:
+        bot.send_message(admin_id, settings.UNSUPPORTED_TYPE,)
+    
+
 @bot.message_handler(commands=['start', 'help'])
 def hello(message):
     try:
@@ -32,25 +69,29 @@ def hello(message):
         logger.exception('Sending Error!')
 
 
-@bot.message_handler(func=lambda mess: mess.from_user.id == admin_id)
-def handle_admin_messages(message: types.Message):
+@bot.message_handler(func=lambda mess: mess.from_user.id == admin_id, content_types=AllTypes())
+def handle_admin_messages(message):
+    logger.info('New message from admin')
     if message.reply_to_message is None:
         try:
+            logger.info('No reply')
             bot.send_message(admin_id, settings.NO_REPLY_MESSAGE)
         except:
             logger.exception('Sending Error')
         return
     try:
         chat_id = message.reply_to_message.forward_from.id
-        # TODO: add other types
-        bot.send_message(chat_id, message.text, parse_mode='Markdown')
+        send_message_content(chat_id, message)
+        logger.info('Send message to user')
     except:
         logger.exception('Replying Error!')
 
-@bot.message_handler(func=lambda mess: mess.from_user.id != admin_id)
-def handle_other_messages(message: types.Message):
+@bot.message_handler(func=lambda mess: mess.from_user.id != admin_id, content_types=AllTypes())
+def handle_other_messages(message):
+    logger.info('New message')
     try:
         bot.forward_message(admin_id, message.chat.id, message.message_id)
+        logger.info('Send message to admin')
     except:
         logger.exception('Forwarding Error!')
 
